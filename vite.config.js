f1d9971e26eba4771d10e5b128949a6071cf15a9
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-09-10 13:07:18 +0300

    ghpages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: "./",
+    base: "/goit-hw-js-09/",
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
